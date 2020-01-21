d5821ad781917c3d74c7166dd16632ce5e820c4e
Chloe Gell <gellert.chloe@gmail.com>
2020-01-21 21:26:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,16 +5,21 @@ def line(katz_deli)
   if(katz_deli.length == 0)
     puts "The line is currently empty."
   else 
-    puts 
+    puts "The line is currently: 1.#{katz_deli[0]} 2."
 end 
 
 def take_a_number(katz_deli, name)
   place = katz_deli.legnth + 1 
-  puts "#{name}, #{place}"
+  puts "Welcome, #{name}. You are number #{place} in line."
 end 
 
-def now_serving()
-  puts ""
-  puts "There is nobody waiting to be served!"
+def now_serving(katz_deli)
+  if(katz_deli.length <= 0)
+    puts "There is nobody waiting to be served!"
+  elsif(katz_deli.length >= 1) 
+    person = katz_deli[0] 
+    puts "Currently serving #{person}"
+    katz_deli.unshift
+  end 
 end 
 
