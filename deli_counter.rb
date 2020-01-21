5774bfae8a7a15550cce072c886a6a69db0b5a47
Chloe Gell <gellert.chloe@gmail.com>
2020-01-21 21:18:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,20 @@
 # Write your code here.
 katz_deli = []
 
-def line 
-  
+def line(katz_deli) 
+  if(katz_deli.length == 0)
+    puts "The line is currently empty."
+  else 
+    puts 
 end 
 
 def take_a_number(katz_deli, name)
   place = katz_deli + 1 
   puts "#{name}, #{place}"
+end 
+
+def now_serving()
+  puts ""
+  puts "There is nobody waiting to be served!"
 end 
 
